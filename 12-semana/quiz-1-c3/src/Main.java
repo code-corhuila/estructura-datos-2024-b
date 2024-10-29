28f1984192092c70ccd080face9c48ff5e070928
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2024-10-28 19:09:05 -0500

    Cambios - 2024-10-28

---
+++ 12-semana/quiz-1-c3/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        public class Nota{
+                private String materia;
+                private double dfTrabajo;
+                private double dfParcial;
+                private double dfAutoCoe;
+                private double NotaDef;
+
+            public Nota(String materia, double dfTrabajo, double dfParcial, double dfAutoCoe, double notaDef) {
+                this.materia = materia;
+                this.dfTrabajo = dfTrabajo;
+                this.dfParcial = dfParcial;
+                this.dfAutoCoe = dfAutoCoe;
+                NotaDef = notaDef;
+            }
+
+            public double getDfTrabajo
+            }
+        }
+
+    }
+}
